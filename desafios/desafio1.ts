88d426e0b82f6c71106cd8534f47b75fdcb44943
KarolKowalczuck <karolinaborgesk@gmail.com>
2022-07-30 23:06:46 -0300

    first commit

---
+++ desafios/desafio1.ts
@@ -0,0 +1,21 @@
+let employee = {
+    code: 10,
+    name: "John"
+};
+
+const employee2: {code:number, name:string} = {
+    code: 20,
+    name: "Luiz"
+}
+
+
+type Employee = {
+    name: string,
+    code: number
+}
+
+const gustavo: Employee = {
+    name: "Gustavo",
+    code: 30
+}
+
---
+++ desafios/desafio2.ts
@@ -0,0 +1,39 @@
+// Como podemos melhorar o esse código usando TS? 
+
+enum Profissao {
+    Atriz,
+    Padeiro
+}
+
+type Pessoa = {
+    nome: string,
+    idade: number,
+    profissao: Profissao
+}
+
+
+let maria: Pessoa = {
+    nome: "Maria",
+    idade: 29,
+    profissao: Profissao.Atriz
+}
+
+
+let roberto: Pessoa = {
+    nome: "Roberto",
+    idade: 19,
+    profissao: Profissao.Padeiro
+}
+
+
+let laura: Pessoa = {
+    nome: "Laura",
+    idade: 32,
+    profissao: Profissao.Atriz
+}
+
+let carlos: Pessoa = {
+    nome: "Carlos",
+    idade: 19,
+    profissao: Profissao.Padeiro
+}
